83403728f7a01e9194d958fc558de4f02348c0d6
MyungRa <thdaudtn93@gmail.com>
2021-06-02 19:25:07 +0900

    covid-20210602

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Moment from 'react-moment'
 
 const Header = () => {
+    const nowDate = new Date();
     return (
         <header className="header">
             <h1>
-                코로나바이러스감염증-19(COVID-19) <span style={{fontSize: 18}}>(~2021/05/31)</span>
+                코로나바이러스감염증-19(COVID-19) 
+                <span style={{fontSize: 18}}>(2020/01/01 ~ <Moment format='YYYY/MM/DD'>{nowDate}</Moment>)</span>
             </h1>
         </header>
     )
